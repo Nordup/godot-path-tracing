03bcb5dabcea2976bfbb2046bfdab32cdecf617d
Nordup <nordup.ondr@gmail.com>
2021-11-27 20:17:22 +0300

    bind compute_pipeline wiht compute_list

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,20 +2,28 @@ extends Node3D
 
 @export_file var shader_comp
 
-
 func _ready():
-	compile_shader()
+#	var r_device = RenderingServer.create_local_rendering_device()
+	var r_device = RenderingServer.get_rendering_device()
+	var c_pipeline_rid = create_compute_pipeline(r_device)
+	create_compute_list(r_device, c_pipeline_rid)
 
 
-func compile_shader():
-	var rendering_device = RenderingServer.get_rendering_device()
+func create_compute_pipeline(r_device: RenderingDevice) -> RID:
 	var shader_source = ShaderTools.create_shader_source(shader_comp)
-	var shader_spirv = rendering_device.shader_compile_spirv_from_source(shader_source)
+	var shader_spirv = r_device.shader_compile_spirv_from_source(shader_source)
 	shader_spirv = ShaderTools.fix_compile_erros(shader_spirv)
 	ShaderTools.print_al_compile_errors(shader_spirv)
 	
-	var shader_rid = rendering_device.shader_create_from_spirv(shader_spirv)
-	var compute_pipeline_rid = rendering_device.compute_pipeline_create(shader_rid)
+	var shader_rid = r_device.shader_create_from_spirv(shader_spirv)
+	var c_pipeline_rid = r_device.compute_pipeline_create(shader_rid)
 	print("Pipeline is valid: " + str(
-		rendering_device.compute_pipeline_is_valid(compute_pipeline_rid)
+		r_device.compute_pipeline_is_valid(c_pipeline_rid)
 		) + "\n")
+	return c_pipeline_rid
+
+
+func create_compute_list(r_device: RenderingDevice, c_pipeline_rid: RID):
+	var c_list = r_device.compute_list_begin()
+	r_device.compute_list_bind_compute_pipeline(c_list, c_pipeline_rid)
+	
