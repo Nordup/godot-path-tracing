3957ff376f6b9a518be3914ad5e328f47b4431e6
Nordup <nordup.ondr@gmail.com>
2021-11-28 16:09:58 +0300

    finally it works!

--- scripts/main.gd
+++ scripts/main.gd
@@ -10,16 +10,20 @@ func _ready():
 	if (not r_device.compute_pipeline_is_valid(cp.rid)):
 		return
 	
-#	compute_list_process(cp)
+	compute_list_process(cp)
 	print("Done in " + str(Time.get_ticks_msec() - msec) + "msec")
+	get_tree().quit()
 
 
 func compute_list_process(cp: ComputePipeline):
-	# Bind needed data here
-	var image_data = PackedByteArray()
-	var buffer_rid = r_device.storage_buffer_create(image_data.size() * 1)
+	var pba = PackedByteArray()
+	pba.resize(64)
+	for i in range(16):
+		pba.encode_u8(i*4, 2.0)
+	
+	var buffer_rid = r_device.storage_buffer_create(64, pba)
 	var uniform = RDUniform.new()
-	uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_SAMPLER
+	uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
 	uniform.binding = 0
 	uniform.add_id(buffer_rid)
 	var u_set_rid = r_device.uniform_set_create([uniform], cp.shader_rid, 0)
@@ -28,9 +32,14 @@ func compute_list_process(cp: ComputePipeline):
 	var c_list = r_device.compute_list_begin()
 	r_device.compute_list_bind_compute_pipeline(c_list, cp.rid)
 	r_device.compute_list_bind_uniform_set(c_list, u_set_rid, 0)
-	r_device.compute_list_dispatch(c_list, 2, 1, 1)
+	r_device.compute_list_dispatch(c_list, 16, 1, 1)
 	r_device.compute_list_end()
 #	r_device.compute_list_add_barrier(c_list)
 
 	r_device.submit()
 	r_device.sync()
+	
+	# Now we can grab our data from the storage buffer
+	var byte_data = r_device.buffer_get_data(buffer_rid)
+	for i in range(16):
+		print(byte_data.decode_u8(i*4))
