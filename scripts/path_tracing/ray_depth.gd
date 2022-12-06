ed4853fdb3eafa709ee6b61f513a9a961f916575
Nordup <nordup.ondr@gmail.com>
2022-12-06 03:17:55 +0300

    fix slider

--- scripts/path_tracing/ray_depth.gd
+++ scripts/path_tracing/ray_depth.gd
@@ -6,20 +6,15 @@ extends Panel
 
 
 func _ready() -> void:
-	slider.drag_ended.connect(_on_drag_ended)
 	slider.value_changed.connect(_on_value_changed)
 	slider.value = path_tracing.ray_depth
 
 
 func _on_value_changed(value: float) -> void:
-	set_text(value)
-
-
-func _on_drag_ended(changed: bool) -> void:
-	if not changed: return
 	path_tracing.ray_depth = slider.value
 	path_tracing.clear_samples()
 	slider.release_focus()
+	set_text(value)
 
 
 func set_text(value: int) -> void:
